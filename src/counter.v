e3d9db96f75d7e31bf25f99488795fe1b932796c
mrmola <25235590+mrmola@users.noreply.github.com>
2024-07-21 04:09:37 -0700

    Fix?

--- src/counter.v
+++ src/counter.v
@@ -12,25 +12,25 @@ module counter (
 );
   always @(posedge clk)
     currentCount[0] <= reset ? 0 : ~currentCount[0];
-  always @posedge(currentCount[0])
+  always @(posedge currentCount[0])
     currentCount[1] <= ~currentCount[1];
-  always @posedge(currentCount[1])
+  always @(posedge currentCount[1])
     currentCount[2] <= ~currentCount[2];
-  always @posedge(currentCount[2])
+  always @(posedge currentCount[2])
     currentCount[3] <= ~currentCount[3];
-  always @posedge(currentCount[3])
+  always @(posedge currentCount[3])
     currentCount[4] <= ~currentCount[4];
-  always @posedge(currentCount[4])
+  always @(posedge currentCount[4])
     currentCount[5] <= ~currentCount[5];
-  always @posedge(currentCount[4])
+  always @(posedge currentCount[4])
     currentCount[5] <= ~currentCount[5];
-  always @posedge(currentCount[4])
+  always @(posedge currentCount[4])
     currentCount[5] <= ~currentCount[5];
-  always @posedge(currentCount[5])
+  always @(posedge currentCount[5])
     currentCount[6] <= ~currentCount[6];
-  always @posedge(currentCount[6])
+  always @(posedge currentCount[6])
     currentCount[7] <= ~currentCount[7];
-  always @negedge(rst_n)
+  always @(negedge rst_n)
     currentCount <= 8'b00000000;
 
 endmodule
